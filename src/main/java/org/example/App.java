8676041d57fd7def6495e5884442494f31fa0103
thach <thachvo530@yahoo.com>
2021-09-11 18:06:35 -0400

    update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,25 +13,24 @@ public class App
 {
     public static void main( String[] args ) {
 
+        Scanner scan = new Scanner(System.in);
+
         System.out.print("What is the principal amount? ");
-        Scanner principal = new Scanner(System.in);
-        int principal1 = principal.nextInt();
+        int principal = scan.nextInt();
 
         System.out.print("What is the rate? ");
-        Scanner rate = new Scanner(System.in);
-        double rate1 = rate.nextDouble();
+        double rate = scan.nextDouble();
 
         System.out.print("What is the number of years? ");
-        Scanner years = new Scanner(System.in);
-        int years1 = years.nextInt();
+        int years = scan.nextInt();
 
         System.out.print("What is the number of times the interest is compounded per year? ");
-        Scanner compound = new Scanner(System.in);
-        int compound1 = compound.nextInt();
+        int compound = scan.nextInt();
 
-        double total = principal1 * Math.pow(( 1 + ((rate1 / 100) / compound1)), (compound1 * years1));
+        double total = principal * Math.pow(( 1 + ((rate / 100) / compound)), (compound * years));
         DecimalFormat df = new DecimalFormat(".00");
 
-        System.out.print("$"+principal1+" invested at "+rate1+"% for "+years1+" years compounded "+compound1+" times per year is $"+df.format(total)+". ");
+        System.out.print("$"+principal+" invested at "+rate+"% for "+years+" years compounded "+compound+" times per year is $"+df.format(total)+". ");
+
     }
 }
